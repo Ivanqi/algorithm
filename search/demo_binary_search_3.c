bef705342eef7b7cf78ca91f6dd2b12009cc0208
Ivanqi <512350225@qq.com>
2019-10-24 15:17:13 +0800

    【优化】增加代码备注

--- search/demo_binary_search_3.c
+++ search/demo_binary_search_3.c
@@ -32,6 +32,13 @@ int binary_search_2(int a[], int n, int value) {
         } else if (a[mid] < value) {
             low = mid + 1;
         } else {
+            /**
+             * 如果mid 等于0，那这个元素已经是数组的第一个元素，那它肯定是我们要找的
+             * 如果mid不等于0，但a[mid]的前一个元素a[mid - 1] 不等于value，那也说明a[mid]就是我们要找的第一个值等于给定值的元素
+             * 
+             * 如果经过检查之后发现a[mid]前面的一个元素a[mid - 1] 也等于value,那说明此时a[mid]肯定不是我们要找的第一个值等于给定的元素
+             * 那我们就更新high = mid - 1, 因为要找的元素肯定出现在[low, mid - 1]之间
+             */
             if ((mid == 0) || (a[mid - 1] != value)) return mid;
             else high = mid - 1;
         }
