246e2f94fe958a604aa59f037c5776837146a27b
Ivanqi <512350225@qq.com>
2019-11-14 21:43:28 +0800

    【开发】增加最大堆-c++版本

---
+++ heap/demo_maxinum_heap_1.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+#define MAX 2000000
+
+int H, A[MAX + 1];
+
+void maxHeadpify(int i) {
+    int l, r, largest;
+    l = 2 * i;
+    r = 2 * i + 1;
+
+    // 从左子节点，自身，右子节点选出值最大的节点
+    if (l <= H && A[l] > A[i]) largest = l;
+    else largest = i;
+
+    if (r <= H && A[r] > A[largest]) largest = r;
+
+    if (largest != i) {
+        swap(A[i], A[largest]);
+        maxHeadpify(largest);
+    }
+}
+
+/**
+ * 10
+ * 4 1 3 2 16 9 10 14 8 7
+ */
+int main() {
+
+    cin >> H;
+
+    for (int i = 1; i <= H; i++) cin >> A[i];
+
+    for (int i = H / 2; i >= 1; i--) maxHeadpify(i);
+
+    for (int i = 1; i <= H; i++) {
+        cout << " " << A[i];
+    }
+    cout << endl;
+
+    return 0;
+}
