65cb38b8d3b083d40dc9bc19580c29bb25832897
Ivanqi <512350225@qq.com>
2019-11-14 23:46:20 +0800

    【开发】增加heap说明以及新的堆算法文件

---
+++ heap/demo_heap_1.cpp
@@ -0,0 +1,125 @@
+#include <iostream>
+#include <assert>
+#include <vector>
+using namespace std;
+
+template<class T>
+struct Less {
+    bool operator()(const T& left, const T& right) const
+    {
+        return left < right;
+    }
+};
+
+template<class T>
+struct Greater {
+    bool operator()(const T& left, const T& right) const
+    {
+        return left > right;
+    }
+};
+
+template<class T, class Compare=Less<T>>
+class Heap 
+{
+    public:
+        Heap() 
+        {
+            // 无参的构造函数(系统不会给无参数构造函数)，开始堆是空的不需要做什么事
+        } 
+        Heap(T* a, size_t n) 
+        {
+            _a.reserve(n);  // 开空间
+            for (size_t i = 0; i < n; ++i) {
+                _a.push_back(a[i]);
+            }
+
+            // 建堆，找最后一个非叶子结点
+            for (int i = ((_a.size() - 1) / 2; i >= 0; --i) {   // 不用size_t, 因为i在这可能等于0，用size_t会死循环
+                adjustDown(i);
+            }
+        }
+
+        // 向下调整
+        void adjustDown(int root)
+        {
+            Compare com;
+            int parent = root;
+            size_t child = parent * 2;  // 左孩子
+            while (child < _a.size()) {
+                // 选出小孩子
+                if (child + 1 < _a.size() && com(_a[child + 1], _a[child])) {
+                    ++child;
+                }
+
+                if (com(_a[child], _a[parent]) {
+                    swap(_a[child], _a[parent]);    // 交换值
+                    parent = child;
+                    child = parent * 2;
+                } else {
+                    break;
+                }
+            }
+        }
+
+        // 向下调整
+        void adjustUp(int child) 
+        {
+            Compare com;
+            int parent = child / 2;
+            while (parent >= 0) {
+                if (com(_a[child], _a[parent]) {
+                    swap(_a[parent], _a[child]);
+                    child = parent;
+                    parent = child / 2;
+                } else {
+                    break;
+                }
+            }
+        }
+
+        // 最后插入
+        void push(const T&x)
+        {
+            _a.push_back(x);
+            adjustUp(_a.size() - 1);
+        }
+
+        // 删除最大数
+        void pop() 
+        {
+            assert(!_a.empty());
+            swap(_a[0], _a[_a.size() - 1]);
+            _a.pop_back();
+            adjustDown(0);
+        }
+
+        // 取顶元素
+        T& top()
+        {
+            assert(!_a.empty());
+            return _a[0];
+        }
+
+        size_t size()
+        {
+            return _a.size();
+        }
+
+        bool empty()
+        {
+            return _a.empty();
+        }
+    private:
+        vector<T> _a;
+};
+
+int main () {
+    
+    int a[] = {10,11,13,12,16,18,15,17,14,19};
+     // Heap<int,Greater<int>> hp1(a,sizeof(a)/sizeof(a[0])); 最大堆
+    // Heap<int,Less<int>> hp1(a,sizeof(a)/sizeof(a[0])); 最小堆
+    Heap<int> hp1(a,sizeof(a)/sizeof(a[0])); // 缺省，最小堆
+    hp1.Push(15);
+    return 0;
+}
