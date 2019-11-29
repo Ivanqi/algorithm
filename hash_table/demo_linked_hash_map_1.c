7a372d3e81371261c5d9eb65c8915b7d1a0efcf9
Ivanqi <512350225@qq.com>
2019-11-29 21:06:55 +0800

    【开发】增加linked_hash_map

---
+++ hash_table/demo_linked_hash_map_1.c
@@ -0,0 +1,6 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+#include <string.h>
+#include "demo_dlist_1.h"
+#include "demo_linked_hash_map_1.h"
---
+++ hash_table/demo_linked_hash_map_1.h
@@ -0,0 +1,27 @@
+#ifndef __LINKED_HASH_MAP__
+#define __LINKED_HASH_MAP__
+
+// 数据存放节点
+typedef struct __linked_hash_map_node {
+    void *key;  // 键
+    void *data; // 数据
+    struct __linked_hash_map_node *next;    // 哈希冲突时，用来挂接后续节点
+    struct list_head Dlist_node;    // 用来挂接双向链表
+} LinkedHashMapNode;
+
+typedef struct _linked_hash_map {
+    LinkedHashMapNode **hTabs;  // 哈希桶
+    struct list_head header;    // 双向循环链表
+    int size;
+    int nel_max;    // 支持最大节点数
+    int (*hash_value)(struct __linked_hash_map *h, const void *key);    // 哈希函数
+    int (*keycmp)(struct __linked_hash_map *h, const void *key1, const void key2);  // 哈希key比较函数,当哈希值一致时使用
+    void (*hash_node_free)(LinkedHashMapNode *node, int flg);   // 用来释放节点内存
+} LinkedHashMap;
+
+typedef int (*hash_value_func)(struct _linked_hash_map *h, const void *key);    // 哈希函数
+typedef int (*keycmp_func)(struct _linked_hash_map *h, const void *key1, const void *key2); // 哈希key比较函数，当哈希数值一致时使用
+typedef void (*hash_node_free_func)(LinkedHashMapNode *node, int flg);
+
+LinkedHashMapNode *LinkedHashMapNode_delet(LinkedHashMap *h, void *key);
+#endif
