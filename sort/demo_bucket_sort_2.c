fa10f466400fbc1f7db9eee62577a8ea6a65e2a3
Ivanqi <512350225@qq.com>
2020-01-25 10:37:51 +0800

    【开发】修复错误地方

--- sort/demo_bucket_sort_2.c
+++ sort/demo_bucket_sort_2.c
@@ -1,99 +1,161 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <ctime>
+#include <algorithm>
 
-struct barrel {   
-    int node[10];   
-    int count;      /* the num of node */  
+#define LEFT_BOUND 0.0
+#define RIGHT_BOUND 100.0
+#define LOWER_BOUND 0.0
+#define UPPER_BOUND 100.0 // 随机点的范围
+
+using namespace std;
+
+class Point     // 点
+{
+    public:
+        int id;
+        double x, y;
+        Point(int index, double x0, double y0): id(index), x(x0), y(y0) {}
 };
 
-void dump(int a[], int size) {
-    int i = 0;
-    printf("\n");
+typedef vector<Point> PointVec;
 
-    for (i = 0; i < size; i++) {
-        printf("%d\t", a[i]);
+bool compx(const Point &a, const Point &b) {
+    if (a.x != b.x) {
+        return a.x < b.x;
     }
-    printf("\n");
+    return a.y < b.y;
 }
 
-void swap (int *a , int *b) {
-    int temp = *a;
-    *a = *b;
-    *b = temp;
+bool compy(const Point &a, const Point &b) {
+    return a.y < b.y;
 }
 
-int parition(int *arr, int p, int r) {
-    int i, j;
-    i = j = p;
+class ClosestPoint
+{
+    PointVec points_vec;
+    int numOfPoint;
 
-    for (; j < r; j++) {
-        if (arr[j] < arr[r]) {
-            if (i != j) {
-                swap(arr + i, arr + j);
+    public:
+        ClosestPoint() 
+        {
+            srand(unsigned(time(0)));
+            double x, y;
+            cout << "请输入测试点个数， 将随机生成散点: ";
+            cin >> numOfPoint;
+
+            for (int i = 0; i < numOfPoint; ++i) {
+                x = LEFT_BOUND + (double)rand() / RAND_MAX * (RIGHT_BOUND - LEFT_BOUND);
+                y = LOWER_BOUND + (double)rand() / RAND_MAX * (UPPER_BOUND - LOWER_BOUND);
+                cout << i + 1 << " (" << x << "," << y << ")" << endl;
+                points_vec.emplace_back(i + 1, x, y);   // 生成点的动态数组
             }
-            i++;
-        }
-    }
-    swap(arr + i, arr + j);
-    return i;
-}
-
-void quickSort(int *arr, int left, int right) {
-    if (left >= right) return ;
-
-    int q = 0;
-    q = parition(arr, left, right);
-    quickSort(arr, left, q - 1);
-    quickSort(arr, q + 1, right);
-}
-
-void bucket_sort(int data[], int size) {
-    struct barrel *pBarrel;
-    int max, min, num, i, j, pos, k;
-
-    max = min = data[0];
-    for (i = 1; i < size; i++) {
-        if (data[i] > max) {
-            max = data[i];
-        } else if (data[i] < min) {
-            min = data[i];
-        }
-    }
-
-    num = (max - min + 1) / 10 + 1;
-
-    pBarrel = (struct barrel *)malloc(sizeof(struct barrel) * num);
-    memset(pBarrel, 0, sizeof(struct barrel) * num);
-
-    for (i = 0; i < size; i++) {
-        k = (data[i] - min + 1) / 10;
-        (pBarrel + k)->node[(pBarrel + k)->count] = data[i];
-        (pBarrel + k)->count++;
-    }
-
-    pos = 0;
-    for (i = 0; i < num; i++) {
-        if ((pBarrel + i)->count != 0) {
-            quickSort((pBarrel + i)->node, 0, ((pBarrel + i)->count) - 1);
         }
 
-        for (j = 0; j < (pBarrel + i)->count; j++) {
-            data[pos++] = (pBarrel + i)->node[j];
+        double dist(const Point &a, const Point &b)
+        {
+            double dx = a.x - b.x;
+            double dy = a.y - b.y;
+            return sqrt(dx * dx - dy * dy); // 返回两点之间的距离
         }
-    }
-    free(pBarrel);
-}
 
-void bucket_sort_test() {
-    int a[] = {78, 17, 39, 26, 72, 94, 21, 12, 23, 91};
-    int size = sizeof(a) / sizeof(int);
-    printf("\n bucket sort test ...");
-    bucket_sort(a, size);
-    dump(a,size);
-}
+        void bfCalDist()    // 暴力求解
+        {
+            size_t num = points_vec.size();
+            if (num <= 1) {
+                cout << "输入个数<=1 !!!" << endl;
+                return ;
+            }
+
+            int i, j, s, t;
+            double distance = RAND_MAX, d;
+            for (i = 0; i < num; i++) {
+                for (j = i + 1; j < num; ++j) {
+                    d = dist(points_vec[i], points_vec[j]);
+                    if (d < distance) {
+                        distance = d;
+                        s = points_vec[i].id;
+                        t = points_vec[j].id;
+                    }
+                }
+            }
+            cout << "点" << s << "到点" << t << "的距离最小：" << distance << endl;
+        }
+
+        double calcDist(size_t left, size_t right, size_t &s, size_t &t)
+        {
+            // 一个点，返回无穷大
+            if (left == right) return RAND_MAX;
+
+            // 两个点，直接计算距离
+            if (left + 1 == right) {
+                s = points_vec[left].id;
+                t = points_vec[right].id;
+                return dist(points_vec[left], points_vec[right]);
+            }
+
+            sort(points_vec.begin() + left, points_vec.begin() + right + 1, compx);
+
+            // 把点群按照x排序
+            size_t mid = (left + right) / 2;
+            double mid_x = points_vec[mid].x;
+            double distance = RAND_MAX, d;
+
+            distance = min(distance, calcDist(left, mid, s, t));    // 递归划分左边
+            distance = min(distance, calcDist(mid + 1, right, s, t)); // 递归划分右边
+
+            size_t i, j, k = 0;
+            PointVec temp;  // 存储临时点（在mid_x左右d范围内的）
+            for (i = left; i <= right; ++i) {
+                if (fabs(points_vec[i].x - mid_x) <= distance) {
+                    temp.emplace_back(points_vec[i].id, points_vec[i].x, points_vec[i].y);
+                    k++;
+                }
+            }
+
+            sort(temp.begin(), temp.end(), compy);    // 再把范围内的点，按y排序
+            for (i = 0; i < k; ++i) {
+                for (j = i + 1; j < k && temp[j].y - temp[i].y < distance; ++j) {
+                    // 在临时点里寻找距离更小的，内层循环最多执行不超过4次就会退出
+                    d = dist(temp[i], temp[j]);
+                    if (d < distance) {
+                        distance = d;
+                        s = temp[i].id;
+                        t = temp[j].id;
+                    }
+                }
+            }
+            return distance;
+        }
+
+        void closestDist() {
+            size_t num = points_vec.size();
+            if(num <= 1) {
+                cout << "输入个数<=1 !!!" << endl;
+                return;
+            }
+
+            size_t s, t; s = t = 0; //记录起终点
+            double d = calcDist(0, num - 1, s, t);
+            cout << "点" << s << "到点" << t << "的距离最小：" << d << endl;
+        }
+};
 
 int main() {
-    bucket_sort_test();
+
+    ClosestPoint cp;
+    clock_t start, end;
+    cout << "方法1，暴力求解：" << endl;
+    start = clock();
+    cp.bfCalDist();
+    end = clock();
+    cout << "耗时：" << (double)(end - start) << "ms." << endl;
+    cout << "-------------------" << endl;
+    cout << "方法2，分治求解：" << endl;
+    start = clock();
+    cp.closestDist();
+    end = clock();
+    cout << "耗时：" << (double)(end - start) << "ms." << endl;
     return 0;
 }
