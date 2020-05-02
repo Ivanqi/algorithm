60d7ce502001d34d9336f41034b240f6389c0f3d
Ivanqi <512350225@qq.com>
2020-05-02 12:34:14 +0800

    【开发】优化bitmap

--- bitmap/demo_bitmap_1.cpp
+++ bitmap/demo_bitmap_1.cpp
@@ -6,22 +6,28 @@ class BitMap
 {
     private:
         vector<int> _bits;
+        int nbits;
     public:
         BitMap(size_t range)
         {
+            nbits = range;
             // 此时多开辟一个空间
             _bits.resize(range / 32 + 1);
+            
         }
 
         void Set(size_t x)
         {
+            if (x > nbits) return;
             int index = x / 32;             // 确定哪个数据(区间)
             int temp = x % 32;              // 确定哪个bit位
+            // cout << "x: " << x << " | index: " << index << " | temp:" << temp << " | 1 << temp:" << (1 << temp) <<  endl;
             _bits[index] |= (1 << temp);    // 位操作即可
         }
 
         void Reset(size_t x)
         {
+            if (x > nbits) return;
             int index = x / 32;
             int temp = x % 32;
             _bits[index] &= ~(1 << temp);   // 取反
@@ -29,6 +35,7 @@ class BitMap
 
         bool get(size_t x)
         {
+            if (x > nbits) 0;
             int index = x / 32;
             int temp = x % 32;
             if (_bits[index] & (1 << temp)) {
@@ -39,20 +46,42 @@ class BitMap
         }
 };
 
+void test_case_1() {
+    BitMap bm(200);
+    bm.Set(1);
+    bm.Set(20);
+    bm.Set(10);
 
-void TestBitMap() {
-    // BitMap am(-1);
+    cout << "1: " << bm.get(1) << endl;
+    cout << "20: " << bm.get(20) << endl;
+    cout << "10: " << bm.get(10) << endl;
+
+    cout << " ----- 删除位数据 ------ " << endl;
+    bm.Reset(1);
+    bm.Reset(20);
+    bm.Reset(10);
+    
+    cout << "1: " << bm.get(1) << endl;
+    cout << "20: " << bm.get(20) << endl;
+    cout << "10: " << bm.get(10) << endl;
+}
+
+void test_case_2() {
     BitMap bm(200);
     bm.Set(136);
-    bm.Set(1);
+    bm.Set(20);
 
     cout << bm.get(136) << endl;
     bm.Reset(136);
     cout << bm.get(136) << endl;
+
+    cout << bm.get(20) << endl;
+    bm.Reset(20);
+    cout << bm.get(20) << endl;
 }
 
 int main() {
 
-    TestBitMap();
+    test_case_1();
     return 0;
 }
--- bitmap/readme.md
+++ bitmap/readme.md
@@ -1,3 +1,4 @@
 #### 参考资料
 - [漫画：Bitmap算法 整合版](https://mp.weixin.qq.com/s/xxauNrJY9HlVNvLrL5j2hg)
 - [C++位图详解（BitMap）](https://blog.csdn.net/archyli/article/details/78573362)
+- [海量数据处理--位图（BitMap）](https://blog.csdn.net/xxpresent/article/details/56488881)
