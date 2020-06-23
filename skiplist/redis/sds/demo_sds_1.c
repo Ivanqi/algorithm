baba374df56a023d7fd5cbe55ab2288b7124a3d9
Ivanqi <512350225@qq.com>
2020-06-23 15:12:10 +0800

    【开发】redis sds新增API 函数

--- skiplist/redis/sds/demo_sds_1.c
+++ skiplist/redis/sds/demo_sds_1.c
@@ -229,30 +229,202 @@ sds sdscatvprintf(sds s, const char *fmt, va_list ap) {
     return t;
 }
 
+// 接受一个SDS和一个C字符串作为参数，从SDS中移除所有在C字符串中出现的字符
+sds sdstrim(sds s, const char *cset) {
+
+    struct sdshdr *sh = (void*)(s - (sizeof(struct sdshdr)));
+    char *start, *end, *sp, *ep;
+    size_t len;
+
+    sp = start = s;
+    ep = end = s + sdslen(s) - 1;
+
+    // 从头开始遍历，如果无法匹配cset中的值，就停下
+    while (sp <= end && strchr(cset, *sp)) {
+        sp++;
+    }
+
+    // 从尾开始往前遍历，如果无法匹配cset中的值，就停下
+    while (ep > start && strchr(cset, *ep)) {
+        ep--;
+    }
+
+    len = (sp > ep) ? 0 : ((ep - sp) + 1);
+    if (sh->buf != sp) {
+        memmove(sh->buf, sp, len);
+    }
+
+    sh->buf[len] = '\0';
+    sh->free = sh->free + (sh->len - len);
+    sh->len = len;
+
+    return s;
+}
+
+// 保留SDS给定区间内的数据，不在区间内的数据会被覆盖或清除
+sds sdsrange(sds s, int start, int end) {
+
+    struct sdshdr *sh = (void *)(s - sizeof(struct sdshdr));
+    size_t newlen, len = sdslen(s);
+
+    if (len == 0) {
+        return s;
+    }
+
+    // 如果start 为负，字符串长度 + start
+    if (start < 0) {
+        start = len + start;
+        if (start < 0) {
+            start = 0;
+        }
+    }
+
+    // 如果start 为负，字符串长度 + end
+    if (end < 0) {
+        end = len + end;
+        if (end < 0) {
+            end = 0;
+        }
+    }
+
+    newlen = (start > end) ? 0 : (end - start) + 1;
+
+    if (newlen != 0) {
+        // start 大于 len的情况
+        if (start >= (signed)len) {
+            newlen = 0;
+        } else if (end >= (signed)len) { // end 大于len的情况
+            newlen = len - 1;
+            newlen = (start > end) ? 0 : (end - start) + 1;
+        }
+    } else {
+        start = 0;
+    }
+
+    if (start && newlen) {
+        memmove(sh->buf, sh->buf + start, newlen);
+    }
+
+    sh->buf[newlen] = '\0';
+    sh->free = sh->free + (sh->len - newlen);
+    sh->len = newlen;
+    return s;
+}
+
+// 对比两个SDS字符串是否相同
+int sdscmp(const sds s1, const sds s2) {
+
+    size_t l1, l2, minlen;
+    int cmp;
+
+    l1 = sdslen(s1);
+    l2 = sdslen(s2);
+    minlen = (l1 < l2) ? l1 : l2;
+    cmp = memcmp(s1, s2, minlen);
+
+    if (cmp == 0) {
+        return l1 - l2;
+    }
+    return cmp;
+}
+
+// 在 sds buf 的右端添加 incr 个位置。如果 incr 是负数时，可以作为 right-trim 使用
+void sdsIncrLen(sds s, int incr) {
+
+    struct sdshdr *sh = (void*) (s - (sizeof(struct sdshdr)));
+
+    assert(sh->free >= incr);
+    sh->len += incr;
+    sh->free -= incr;
+    assert(sh->free >= 0);
+    s[sh->len] = '\0';
+}
+
 int main() {
+    {
+        struct sdshdr *sh;
+        sds x = sdsnew("foo"), y;
 
-    struct sdshdr *sh;
-    sds x = sdsnew("foo"), y;
+        test_cond("创建字符串并获取长度", sdslen(x) == 3 && memcmp(x,"foo\0",4) == 0);
+        sdsfree(x);
 
-    test_cond("创建字符串并获取长度", sdslen(x) == 3 && memcmp(x,"foo\0",4) == 0);
-    sdsfree(x);
+        x = sdsnewlen("foo", 2);
+        test_cond("创建指定长度的字符串", sdslen(x) == 2 && memcmp(x,"fo\0",2) == 0);
 
-    x = sdsnewlen("foo", 2);
-    test_cond("创建指定长度的字符串", sdslen(x) == 2 && memcmp(x,"fo\0",2) == 0);
+        x = sdscat(x, "bar");
+        test_cond("字符串连接", sdslen(x) == 5 && memcmp(x, "fobar\0", 6) == 0);
 
-    x = sdscat(x, "bar");
-    test_cond("字符串连接", sdslen(x) == 5 && memcmp(x, "fobar\0", 6) == 0);
+        x = sdscpy(x, "a");
+        test_cond("sdscpy() 复制一个短字符串", sdslen(x) == 1 && memcmp(x, "a\0", 2) == 0);
 
-    x = sdscpy(x, "a");
-    test_cond("sdscpy() 复制一个短字符串", sdslen(x) == 1 && memcmp(x, "a\0", 2) == 0);
+        x = sdscpy(x, "xyzxxxxxxxxxxyyyyyyyyyykkkkkkkkkk");
+        test_cond("sdscpy() 复制一个长字符串", sdslen(x) == 33 && memcmp(x, "xyzxxxxxxxxxxyyyyyyyyyykkkkkkkkkk\0", 34) == 0);
+        sdsfree(x);
 
-    x = sdscpy(x, "xyzxxxxxxxxxxyyyyyyyyyykkkkkkkkkk");
-    test_cond("sdscpy() 复制一个长字符串", sdslen(x) == 33 && memcmp(x, "xyzxxxxxxxxxxyyyyyyyyyykkkkkkkkkk\0", 34) == 0);
-    sdsfree(x);
+        x = sdscatprintf(sdsempty(), "%d", 123);
+        test_cond("sdscatprintf() 基础例子", strlen(x) == 3 && memcmp(x, "123\0", 4) == 0);
+        sdsfree(x);
 
-    x = sdscatprintf(sdsempty(), "%d", 123);
-    test_cond("sdscatprintf() 基础例子", strlen(x) == 3 && memcmp(x, "123\0", 4) == 0);
-    sdsfree(x);
+        x = sdstrim(sdsnew("xxciaoyy"), "xy");
+        test_cond("sdstrim() 正确修改字符", strlen(x) == 4 && memcmp(x, "ciao\0", 5) == 0);
 
+        y = sdsrange(sdsdup(x), 1, 1);
+        test_cond("sdsrange(..., 1, 1)", sdslen(y) == 1 && memcmp(y, "i\0", 2) == 0);
+        sdsfree(y);
+
+        y = sdsrange(sdsdup(x), -2, -1);
+        test_cond("sdsrange(..., -2, -1)", sdslen(y) == 3 && memcmp(y, "iao\0", 4) == 0);
+        sdsfree(y);
+
+        y = sdsrange(sdsdup(x), 2, 1);
+        test_cond("sdsrange(..., 2, 1)", sdslen(y) == 0 && memcmp(y, "\0", 1) == 0);
+        sdsfree(y);
+
+        y = sdsrange(sdsdup(x), 1, 100);
+        test_cond("sdsrange(..., 1, 100)", sdslen(y) == 3 && memcmp(y, "iao\0", 4) == 0);
+        sdsfree(y);
+
+        y = sdsrange(sdsdup(x), 100, 100);
+        test_cond("sdsrange(..., 100, 100)", sdslen(y) == 0 && memcmp(y, "\0", 1) == 0);
+        sdsfree(y);
+        sdsfree(x);
+
+        x = sdsnew("foo");
+        y = sdsnew("foa");
+        test_cond("sdscmp(foo, foa)不相等", sdscmp(x, y) > 0);
+        sdsfree(x);
+        sdsfree(y);
+
+        x = sdsnew("bar");
+        y = sdsnew("bar");
+        test_cond("sdscmp(bar, bar)相等", sdscmp(x, y) == 0);
+        sdsfree(x);
+        sdsfree(y);
+
+        x = sdsnew("aar");
+        y = sdsnew("bar");
+        test_cond("sdscmp(aar, bar)不相等", sdscmp(x, y) < 0);
+
+        {
+            int oldfree;
+            sdsfree(x);
+            x = sdsnew("0");
+            sh = (void *) (x - (sizeof(struct sdshdr)));
+            test_cond("sdsnew() free/len buffers", sh->len == 1 && sh->free == 0);
+
+            x = sdsMakeRoomFor(x, 1);
+            sh = (void*) (x - (sizeof(struct sdshdr)));
+            test_cond("sdsMakeRoomFor()", sh->len == 1 && sh->free > 0);
+            oldfree = sh->free;
+
+            x[1] = '1';
+            sdsIncrLen(x, 1);
+            test_cond("sdsIncrLen() -- content", x[0] == '0' && x[1] == '1');
+            test_cond("sdsIncrLen() -- len", sh->len == 2);
+            test_cond("sdsIncrLen() -- free", sh->free == oldfree - 1);
+        }
+    }
+
+    test_report();
     return 0;
 }
