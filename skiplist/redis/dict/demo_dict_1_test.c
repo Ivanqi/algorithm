808eb3acc81fd189e8bd6edf1121b391b06f4bd1
Ivanqi <512350225@qq.com>
2020-07-03 21:18:35 +0800

    【开发】redis dict 添加新测试案例

--- skiplist/redis/dict/demo_dict_1_test.c
+++ skiplist/redis/dict/demo_dict_1_test.c
@@ -27,9 +27,35 @@ void test_case_1() {
 
     d = dictCreate(type, privDataPtr);
 
+    int one = 1;
+    int two = 2;
+
+    dictAdd(d, (void *)&one, (void *)&one);
+    dictAdd(d, (void *)&two, (void *)&two);
+
+    dictIterator *dIter = dictGetIterator(d);
+    dictEntry *node;
+
+    while ((node = dictNext(dIter)) != NULL) {
+        printf("node key:%d, val:%d\n", *(void **)node->key, *(void **)node->v.val);
+    }
+    dictRelease(d);
+}
+
+void test_case_2() {
+    dictType *type = (dictType *)malloc(sizeof(dictType));
+
+    char privDataPtr[] = "privDataPtr";
+    dict *d;
+
+    type->hashFunction = hashFunc_1;
+
+    d = dictCreate(type, privDataPtr);
+
+    int num[] = {1, 2, 3, 4, 5, 6, 7};
     int i;
-    for (i = 1; i < 7; i++) {
-        dictAdd(d, (void*)&i, (void*)&i);
+    for (i = 0; i < 7; i++) {
+        dictAdd(d, (void*)&num[i], (void*)&num[i]);
     }
 
     dictIterator *dIter = dictGetIterator(d);
@@ -47,11 +73,77 @@ void test_case_1() {
     } else {
         printf("没找到对应的值\n");
     }
+
     dictRelease(d);
 }
 
+int strCompare (void *privdata, const void *key1, const void *key2) {
+
+    return strcmp(key1, key2) == 0 ? 1 : 0;
+}
+
+void test_case_3() {
+    dictType *type = (dictType *)malloc(sizeof(dictType));
+
+    char privDataPtr[] = "privDataPtr";
+    dict *d;
+
+    type->hashFunction = hashFunc_3;
+    type->keyCompare = strCompare;
+
+
+    d = dictCreate(type, privDataPtr);
+
+    printf("\n================= add =================\n");
+    dictAdd(d, "one", "one");
+    dictAdd(d, "two", "two");
+    dictAdd(d, "three", "three");
+    dictAdd(d, "four", "four");
+    dictAdd(d, "five", "five");
+    dictAdd(d, "six", "six");
+
+    printf("\n================= fetch =================\n");
+    dictIterator *dIter = dictGetIterator(d);
+    dictEntry *node;
+    while ((node = dictNext(dIter)) != NULL) {
+        printf("node key:%s, val:%s\n", (char *)node->key, (char *)node->v.val);
+    }
+
+    printf("\n================= find =================\n");
+    dictEntry *entry = dictFind(d, "two");
+    if (entry) {
+       printf("find key:%s, val:%s\n", (char *)entry->key, (char *)entry->v.val);
+    } else {
+        printf("没找到对应的值\n");
+    }
+
+    printf("\n================= delete val =================\n");
+    void *ret = dictFetchValue(d, "six");
+    if (ret) {
+        printf("find val:%s\n", (char *)ret);
+    }
+
+    printf("\n================= fetch =================\n");
+    dictDelete(d, "four");
+    dIter = dictGetIterator(d);
+     while ((node = dictNext(dIter)) != NULL) {
+        printf("node key:%s, val:%s\n", (char *)node->key, (char *)node->v.val);
+    }
+
+    printf("\n================= random =================\n");
+    entry = dictGetRandomKey(d);
+     if (entry) {
+       printf("find key:%s, val:%s\n", (char *)entry->key, (char *)entry->v.val);
+    } else {
+        printf("没找到对应的值\n");
+    }
+
+    dictRelease(d);
+}
+
+
 int main() {
 
-    test_case_1();
+    test_case_3();
     return 0;
 }
